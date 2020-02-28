66f60fa2cc6fbe25173f904a733fff5a973a1b48
orlyohreally <orli-knop@ya.ru>
2020-02-28 13:56:51 +0200

    Added title for main page

--- client/src/app/modules/home/home-routing.module.ts
+++ client/src/app/modules/home/home-routing.module.ts
@@ -5,7 +5,8 @@ import { HomePageComponent } from './home-page/home-page.component';
 const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent
+    component: HomePageComponent,
+    data: { title: 'Main Page' }
   }
 ];
 

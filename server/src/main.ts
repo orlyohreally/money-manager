19870f0f3bd71507445d8c9be13ed222758df68c
orlyohreally <orli-knop@ya.ru>
2020-04-27 19:05:39 +0300

    Enabled cors for localhost for staging

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,11 @@
 import "module-alias/register";
 
 import * as bodyParser from "body-parser";
+import * as cors from "cors";
 import * as express from "express";
 import * as mongoose from "mongoose";
 import * as path from "path";
+
 import { familiesRouter } from "./services/families";
 import { paymentSubjectsRouter } from "./services/payment-subjects";
 import { paymentsRouter } from "./services/payments";
@@ -23,6 +25,26 @@ const runServer = async () => {
   app.listen(port, () => {
     console.log("Example app listening on port 3000!");
   });
+
+  if (process.env.NODE_ENV === "staging") {
+    const whitelist = ["localhost:4200", process.env.BACK_END_URL];
+    const corsOptions = {
+      origin: (
+        origin: string | undefined,
+        callback: (err: Error | null, allow?: boolean) => void
+      ) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+          // tslint:disable-next-line: no-null-keyword
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      // some legacy browsers (IE11, various SmartTVs) choke on 204
+      optionsSuccessStatus: 200
+    };
+    app.use(cors(corsOptions));
+  }
 
   app.use(bodyParser.json({ limit: "10mb" }));
   app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));

06deb80e73552f5175258424a23fc31f357a2a84
orlyohreally <orli-knop@ya.ru>
2020-04-27 19:25:31 +0300

    Removed files tracked file in gitignore

--- shared/types/family-member.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Roles;
-(function (Roles) {
-    Roles["Owner"] = "Owner";
-    Roles["Admin"] = "Admin";
-    Roles["Member"] = "Member";
-})(Roles = exports.Roles || (exports.Roles = {}));
-//# sourceMappingURL=family-member.js.map
--- shared/types/payment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// /families/:id
-// /families/:id/payments
-// /families/:id/payment-subjects
-// /families/:id/members
-//# sourceMappingURL=payment.js.map

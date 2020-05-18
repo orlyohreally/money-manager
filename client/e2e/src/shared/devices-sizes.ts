96b4d7f2a63ab7f6140473d8dddb957822459ec0
orlyohreally <orli-knop@ya.ru>
2020-05-18 14:21:00 +0300

    refactor: protractor browser sizes are imported from file

---
+++ client/e2e/src/shared/devices-sizes.ts
@@ -0,0 +1,9 @@
+export const iphone5 = {
+  height: 568,
+  width: 320
+};
+
+export const tablet = {
+  height: 738,
+  width: 768
+};
--- client/e2e/src/shared/index.ts
+++ client/e2e/src/shared/index.ts
@@ -1,4 +1,5 @@
 export * from './app-urls';
+export * from './devices-sizes';
 export * from './functions.spec';
 export * from './reset-tested-user.spec';
 export * from './server-calls.spec';

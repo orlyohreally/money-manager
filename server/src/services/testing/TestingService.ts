971db95662f208d352dc2eb145e481780dd32cf6
orlyohreally <orli-knop@ya.ru>
2020-05-19 23:07:20 +0300

    feat: tested server posts 2 payment subjects on start

--- server/src/services/testing/TestingService.ts
+++ server/src/services/testing/TestingService.ts
@@ -1,6 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 
+import { PaymentSubject } from "@shared/types";
+// tslint:disable-next-line: max-line-length
+import { PaymentSubjectsService } from "@src/services/payment-subjects/PaymentSubjectsService";
+
 export class TestingService {
+  private paymentSubjectsService: PaymentSubjectsService;
+
+  constructor({
+    paymentSubjectsService
+  }: {
+    paymentSubjectsService: PaymentSubjectsService;
+  }) {
+    this.paymentSubjectsService = paymentSubjectsService;
+  }
   public async validateCredentials(
     req: Request,
     res: Response,
@@ -17,5 +30,20 @@ export class TestingService {
     } catch (error) {
       return res.status(401).json({ message: "Unauthorized access" });
     }
+  }
+
+  public async prepareDBForTests() {
+    const paymentSubjects: Pick<PaymentSubject, "name" | "icon">[] = [
+      {
+        name: "apartment",
+        icon: "apartment"
+      },
+      {
+        name: "pet",
+        icon: "pet"
+      }
+    ];
+    await this.paymentSubjectsService.createPaymentSubject(paymentSubjects[0]);
+    await this.paymentSubjectsService.createPaymentSubject(paymentSubjects[1]);
   }
 }
--- server/src/services/testing/index.ts
+++ server/src/services/testing/index.ts
@@ -1,11 +1,14 @@
 // tslint:disable-next-line: max-line-length
 import { familiesService } from "@src/services/families";
+import { paymentSubjectsService } from "@src/services/payment-subjects";
 import { paymentsService } from "@src/services/payments";
 import { TestingRouter } from "@src/services/testing/TestingRouter";
 import { TestingService } from "@src/services/testing/TestingService";
 import { usersService } from "@src/services/users";
 
-export const testingService = new TestingService();
+export const testingService = new TestingService({
+  paymentSubjectsService: paymentSubjectsService
+});
 export const testingRouter = new TestingRouter({
   service: testingService,
   usersService: usersService,

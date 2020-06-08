b89f98f13d57d912f93f860ed4b5134a4522985f
orlyohreally <orly.knop@gmail.com>
2020-06-09 00:17:20 +0300

    fix: fixed gaMeasurementId value

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   backendURL: '/api/v1',
   supportEmail: 'orly.knop@gmail.com',
-  gaMeasurementId: 'xx-xxxxxxxxx-x'
+  gaMeasurementId: 'UA-168919876-1'
 };

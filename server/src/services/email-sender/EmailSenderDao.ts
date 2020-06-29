d393acc74931f0fe2269176cd012f8f6dc509d5b
orlyohreally <orly.knop@gmail.com>
2020-06-29 22:20:03 +0300

    fix: removed unnecessary code in email sender dao

--- server/src/services/email-sender/EmailSenderDao.ts
+++ server/src/services/email-sender/EmailSenderDao.ts
@@ -1,28 +1,14 @@
 import * as sgMail from "@sendgrid/mail";
-import { RequestResponse } from "request";
+
 import { IEmailSenderDao } from "./EmailSenderService";
 
 export class EmailSenderDao implements IEmailSenderDao {
-  constructor() {
-    this.configSendGrid();
-  }
-
-  public testEmail(to: string): Promise<[RequestResponse, {}]> {
-    const msg = {
-      to: to,
-      from: "test@example.com",
-      subject: "Sending with Twilio SendGrid is Fun",
-      text: "and easy to do anywhere, even with Node.js",
-      html: "<strong>and easy to do anywhere, even with Node.js</strong>"
-    };
-    return sgMail.send(msg);
-  }
-
   public async sendEmail(
     templateId: string,
     to: string,
     dynamicTemplateData: { [key: string]: string | number }
   ): Promise<void> {
+    this.configSendGrid();
     const msg = {
       to: to,
       from: "orly.knop@gmail.com",
